da6153734ed073a944def1f26d9dcfe763d540a5
pH <hericles_peixer@hotmail.com>
2019-03-27 19:58:17 -0300

    subindo arquivo do codigo

---
+++ remedios.h
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 100
+
+int main(){
+	
+};
+
+void inicializar(LISTA* lista) {
+	
+}; 
+
+bool carregar(LISTA*) {
+	
+};
+
+void exibir(LISTA* lista) {
+	
+};
+
+int tamanho(LISTA* lista) {
+	
+};
+
+int buscar(LISTA* lista, TIPOCHAVE chave) {
+	
+}; 
+
+bool excluir(LISTA* lista, TIPOCHAVE chave) {
+
+}; 
+
+void reinicializarLista(LISTA* lista) {
+	
+}; 
+
+bool inserir(LISTA* l, REGISTRO registro) {
+	
+}; 
